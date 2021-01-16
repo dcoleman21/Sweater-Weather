8b6791e990a3a2f9b314f448a8ed87c071cc61d4
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 16:07:43 -0700

    Add missing commas in attr_reader and pass in current, daily and hourly as arguments in the initialize method

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,11 +1,11 @@
 class Forecast
-  attr_reader :current_weather
-              :daily_weather
+  attr_reader :current_weather,
+              :daily_weather,
               :hourly_weather
 
-  def initialize(data)# {:lat=>39.801122, :lng=>-105.081451}
-    @current_weather = CurrentWeather.new(data[:current])
-    @daily_weather = DailyWeather.new(data[:daily])
-    @hourly_weather = HourlyWeather.new(data[:hourly])
+  def initialize(current, daily, hourly)
+    @current_weather = current
+    @daily_weather = daily
+    @hourly_weather = hourly
   end
 end
