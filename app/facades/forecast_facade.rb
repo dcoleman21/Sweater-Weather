cc64691a8272df3818bee133ac4005988d859a9d
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:18:31 -0700

    Change forecst facade to call only the mapfacade and create a forecast poro passing in the latlng coordinates

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,6 @@
 class ForecastFacade
   def self.forecast_by_coords(location)
-    latlng = MapService.get_coords_by_location(location)
-    Forecast.new(ForecastService.forecast_by_coords(latlng[:lat], latlng[:lng]))
+    latlng = MapFacade.get_coords_by_location(location)
+    Forecast.new(latlng)
   end
 end
