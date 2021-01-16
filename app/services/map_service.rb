b7492918ede6a33109d6da31e449cf3fed9edae4
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:08:48 -0700

    Create map service that calls the mapquest api with params location (city,state) that gives the lat and lon

---
+++ app/services/map_service.rb
@@ -0,0 +1,23 @@
+class MapService
+  def self.get_coords_by_location(location)
+    response = conn.get("/geocoding/v1/address") do |r|
+      r.params['location'] = location
+    end
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    parse_lat_lng(parsed)
+  end
+
+  def self.conn
+    Faraday.new("http://www.mapquestapi.com/") do |f|
+      f.params['key'] = ENV['MAP_QUEST_KEY']
+    end
+  end
+
+  def self.parse_lat_lng(response)
+    if response[:results][0][:locations].empty?
+      {lat: 'no match', lng: 'no match'}
+    else
+      response[:results][0][:locations][0][:latLng]
+    end
+  end
+end
