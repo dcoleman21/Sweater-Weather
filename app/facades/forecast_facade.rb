43e418ccfb62d36be37d5657c5e25dac381736ed
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:06:33 -0700

    Create a forecast facade that calls the map service to get the lat and lon based on location then creates a forecast poro passing in the forecast service

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,7 @@
+class ForecastFacade
+  def self.forecast_by_coords(location)
+    latlng = MapService.get_coords_by_location(location)
+    Forecast.new(ForecastService.forecast_by_coords(latlng[:lat], latlng[:lng]))
+    require "pry"; binding.pry
+  end
+end
