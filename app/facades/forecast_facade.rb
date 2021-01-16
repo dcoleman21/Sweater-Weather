b9e86e0ac7a51f643712adea7ab1b57d0003f9f8
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:39:07 -0700

    Change to pass the forecast service which has the open weather data and pass it the lat and lon of the destination

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,6 @@
 class ForecastFacade
-  def self.forecast_by_coords(location)
-    latlng = MapFacade.get_coords_by_location(location)
-    Forecast.new(latlng)
+  def self.forecast_by_coords(location)#"arvada,co"
+    latlng = MapFacade.get_coords_by_location(location)#{:lat=>39.801122, :lng=>-105.081451}
+    Forecast.new(ForecastService.forecast_by_coords(latlng[:lat], latlng[:lng]))
   end
 end
