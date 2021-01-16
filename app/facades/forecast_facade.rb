00d034c320fa9a256b610a56f169a5dc33cdb26b
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:12:12 -0700

    Create forecst facade that calls the map service to get latlng, creates a forecast poro passing the forecast service method to get lat and lon

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,6 +2,5 @@ class ForecastFacade
   def self.forecast_by_coords(location)
     latlng = MapService.get_coords_by_location(location)
     Forecast.new(ForecastService.forecast_by_coords(latlng[:lat], latlng[:lng]))
-    require "pry"; binding.pry
   end
 end
