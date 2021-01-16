465d6f9dc10052c2275c9a0e3c29914a2e4e32ef
Dani Coleman <danicoleman00@gmail.com>
2021-01-16 13:59:07 -0700

    Add zero index to access the array of daily weather info

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -8,13 +8,13 @@ class DailyWeather
               :icon
 
   def initialize(data)
-    @date = convert_to_date(data[:dt])
-    @sunrise = convert_to_datetime(data[:sunrise]
-    @sunset = convert_to_datetime(data[:sunset]
-    @max_temp = data[:temp][:max]
-    @min_temp = data[:temp][:min]
-    @conditions = data[:weather][0][:description]
-    @icon = data[:weather][0][:icon]
+    @date = convert_to_date(data[0][:dt])
+    @sunrise = convert_to_datetime(data[0][:sunrise])
+    @sunset = convert_to_datetime(data[0][:sunset])
+    @max_temp = data[0][:temp][:max]
+    @min_temp = data[0][:temp][:min]
+    @conditions = data[0][:weather][0][:description]
+    @icon = data[0][:weather][0][:icon]
   end
 
   def convert_to_date(unix_timestamp)
