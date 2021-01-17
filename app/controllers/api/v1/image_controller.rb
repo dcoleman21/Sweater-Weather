83455334588d065f5ca6c35b2c3d929b505f4b25
Dani Coleman <danicoleman00@gmail.com>
2021-01-17 14:30:32 -0700

    Refactor controller into a facade, service and poro

--- app/controllers/api/v1/image_controller.rb
+++ app/controllers/api/v1/image_controller.rb
@@ -1,21 +1,6 @@
 class Api::V1::ImageController < ApplicationController
   def show
-    location = params[:location]
-
-    conn = Faraday.new(url: "https://api.unsplash.com") do |f|
-      f.params['query'] = location
-      f.params['page'] = 1
-      f.params['per_page'] = 1
-      f.params['client_id'] = ENV['IMAGE_KEY']
-    end
-    response = conn.get('/search/photos')
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    @image = fetch_image_data(json, location)
-    render json: ImageSerializer.new(@image)
-  end
-
-  def fetch_image_data(json, location)
-    Image.new(json[:results], location)
+    image = ImageFacade.fetch_image(params[:location])
+    render json: ImageSerializer.new(image)
   end
 end
