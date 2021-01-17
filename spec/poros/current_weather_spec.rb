a3025ef1c68b8e6708fee9b53f047699e720c58a
Dani Coleman <danicoleman00@gmail.com>
2021-01-17 10:00:40 -0700

    Add poro test for current weather

--- spec/poros/current_weather_spec.rb
+++ spec/poros/current_weather_spec.rb
@@ -8,14 +8,14 @@ describe "Current Weather Poro" do
 
     expect(current_weather).to be_a(CurrentWeather)
     expect(current_weather.datetime).to be_a(Time)
-    expect(currrent_weather.sunrise).to be_a(Time)
-    expect(currrent_weather.sunset).to be_a(Time)
-    expect(currrent_weather.temperature).to be_a(Numeric)
-    expect(currrent_weather.feels_like).to be_a(Numeric)
-    expect(currrent_weather.humidity).to be_a(Numeric)
-    expect(currrent_weather.uvi).to be_a(Numeric)
-    expect(currrent_weather.visibility).to be_a(Numeric)
-    expect(currrent_weather.conditions).to be_a(String)
-    expect(currrent_weather.icon).to be_a(String)
+    expect(current_weather.sunrise).to be_a(Time)
+    expect(current_weather.sunset).to be_a(Time)
+    expect(current_weather.temperature).to be_a(Numeric)
+    expect(current_weather.feels_like).to be_a(Numeric)
+    expect(current_weather.humidity).to be_a(Numeric)
+    expect(current_weather.uvi).to be_a(Numeric)
+    expect(current_weather.visibility).to be_a(Numeric)
+    expect(current_weather.conditions).to be_a(String)
+    expect(current_weather.icon).to be_a(String)
   end
 end
