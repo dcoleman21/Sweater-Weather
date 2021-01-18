f6d49c0ae1cd1def4b9a9b382021a24c4c56e9a9
Dani Coleman <danicoleman00@gmail.com>
2021-01-18 11:13:52 -0700

    Add method to get road trip time and return impossible route if necessary

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -2,4 +2,13 @@ class MapFacade
   def self.get_coords_by_location(location)
     MapService.get_coords_by_location(location)
   end
+
+  def self.time_between_locations(origin, destination)
+    result = MapService.route(origin, destination)
+    if result[:info][:statuscode] == 402
+      'impossible route'
+    else
+      result[:route][:formattedTime]
+    end
+  end
 end
