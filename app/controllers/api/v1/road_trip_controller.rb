d10e1f0f34006d84e47e9b564db162cfff4907a5
Dani Coleman <danicoleman00@gmail.com>
2021-01-18 13:19:17 -0700

    Change error message

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RoadTripController < ApplicationController
   def create
     user = UserFacade.auth_key(params[:api_key])
     if user.nil?
-      render json: { error: 'invalid key' }, status: 401
+      render json: { error: 'Unable To Authenticate' }, status: 401
     else
       trip = RoadTripFacade.trip_info(params[:origin], params[:destination])
       render json: RoadTripSerializer.new(trip)
