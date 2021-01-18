4d9580d5dc82c326b0c60fd4345706d98576656f
Dani Coleman <danicoleman00@gmail.com>
2021-01-17 17:25:29 -0700

    Refactor create action using a facade

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = UserFacade.create(user_params)
+    if user.id.nil?
+      render json: { errors: user.errors.full_messages.to_sentence }, status: 401
+    else
+      render json: UsersSerializer.new(user), status: 201
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
