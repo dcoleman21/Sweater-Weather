da7330f1d925920f97c4942c2312ee081938e03f
Dani Coleman <danicoleman00@gmail.com>
2021-01-18 12:54:01 -0700

    Add call to forecast service to get weather info and call roadtrip poro passing in origin, destination, travel time and weather info

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,9 +2,7 @@ class RoadTripFacade
   def self.trip_info(origin, destination)
     travel_time = MapFacade.time_between_locations(origin, destination)
     coords = MapFacade.get_coords_by_location(destination)
-    weather_info = WeatherFacade.hourly_forecast(coords.latitude, coords.longitude)
-    require "pry"; binding.pry
-    # weather = weather_info[travel_time.second]
-    # RoadTrip.new(origin, destination, travel_time.first, weather)
+    weather_info = ForecastService.forecast_by_coords(coords[:lat], coords[:lng])
+    RoadTrip.new(origin, destination, travel_time, weather_info)
   end
 end
