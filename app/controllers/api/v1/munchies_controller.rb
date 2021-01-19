0628379019a1fb952f2bd90b791d0784691ae8a8
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 09:33:34 -0700

    Make the api call to the yelp businesses/search endpoint. Create method to fetch restaurant data and create a new munchie poro

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,30 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
+    start = params[:start]
+    dest = params[:end]
+    food = params[:food]
 
+    conn = Faraday.new(url: "https://api.yelp.com") do |faraday|
+      faraday.headers['Authorization'] = "Bearer #{ENV['YELP_KEY']}"
+      # f.params['location'] = dest
+      # f.params['categories'] = "restaurants,chinese"
+      # f.params['term'] = food
+      # f.params['limit'] = 3
+    end
+    response = conn.get("/v3/businesses/search") do |req|
+      req.params['location'] = dest
+      req.params['categories'] = "restaurants,chinese"
+      req.params['term'] = food
+      req.params['limit'] = 3
+    end
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    @munchies = fetch_restaurant_data(parsed)
+  end
+
+  def fetch_restaurant_data(parsed)
+    parsed[:businesses].map do |data|
+      require "pry"; binding.pry
+      Munchie.new(data)
+    end
   end
 end
