da60a069caf6d1d63b36c656cc325d2b7720b028
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 10:24:07 -0700

    Add helper methods to nest the forecast and restaurant. Convert time to hours and mins. Get the restaurant address in a single line

--- app/poros/munchie.rb
+++ app/poros/munchie.rb
@@ -5,6 +5,29 @@ class Munchie
               :restaurant
 
   def initialize(destination_city, travel_time, forecast, restaurant)
-    require "pry"; binding.pry
+    @destination_city = destination_city
+    @travel_time = time_in_hours_and_mins(travel_time)
+    @forecast = weather_at_arrival(forecast)
+    @restaurant = restaurant_info(restaurant)
+  end
+
+  def weather_at_arrival(forecast)
+    {
+      summary: forecast.hourly_weather[1].conditions,
+      temperature: forecast.hourly_weather[1].temperature
+    }
+  end
+
+  def time_in_hours_and_mins(travel_time)
+    hour = travel_time.slice(1, 1)
+    minutes = travel_time.slice(3, 2)
+    "#{hour} hour #{minutes} min"
+  end
+
+  def restaurant_info(restaurant)
+    {
+      name: restaurant[:businesses][0][:name],
+      address: restaurant[:businesses][0][:location][:display_address].join(", ")
+    }
   end
 end
