f87d5f3c252c8def4d8ac8273d0d9703476e9369
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 10:01:25 -0700

    Call mapfacade, forecastfacade and create a munchie poro passing in the dest_city, travel_time, forecast and restauraunt results

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    start = params[:start]
-    dest = params[:end]
+    start_city = params[:start]
+    destination_city = params[:end]
     food = params[:food]
 
     conn = Faraday.new(url: "https://api.yelp.com") do |faraday|
@@ -12,19 +12,21 @@ class Api::V1::MunchiesController < ApplicationController
       # f.params['limit'] = 3
     end
     response = conn.get("/v3/businesses/search") do |req|
-      req.params['location'] = dest
+      req.params['location'] = destination_city
       req.params['categories'] = "restaurants,chinese"
       req.params['term'] = food
       req.params['limit'] = 3
     end
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    @munchies = fetch_restaurant_data(parsed)
+    restaurant = JSON.parse(response.body, symbolize_names: true)
+    travel_time = MapFacade.time_between_locations(start_city, destination_city)
+    forecast = ForecastFacade.forecast_by_coords(destination_city)
+    Munchie.new(destination_city, travel_time, forecast, restaurant)
+    render json: MunchieSerializer.new(munchies)
   end
 
-  def fetch_restaurant_data(parsed)
-    parsed[:businesses].map do |data|
-      require "pry"; binding.pry
-      Munchie.new(data)
-    end
-  end
+  # def fetch_restaurant_data(restaurant)
+  #   restaurant[:businesses].map do |data|
+  #     Munchie.new(data)
+  #   end
+  # end
 end
