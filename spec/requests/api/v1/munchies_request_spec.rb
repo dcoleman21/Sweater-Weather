e91163f422160dae28884c791e324c84780836d9
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 10:27:35 -0700

    Finish the munchie request spec

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -9,7 +9,37 @@ describe " Munchies API" do
 
       get "/api/v1/munchies?start=#{origin}&end=#{destination}&food=#{food}"
       expect(response).to be_successful
-      parsed = JSON.parse(response.body, symbolize_names: true)
+      munchie = JSON.parse(response.body, symbolize_names: true)
+
+      expect(munchie).to be_a(Hash)
+      expect(munchie).to have_key(:data)
+      expect(munchie[:data]).to be_a(Hash)
+
+      expect(munchie[:data]).to have_key(:id)
+      expect(munchie[:data][:id]).to eq(nil)
+      expect(munchie[:data]).to have_key(:type)
+      expect(munchie[:data][:type]).to eq('munchie')
+      expect(munchie[:data]).to have_key(:attributes)
+      expect(munchie[:data][:attributes]).to be_a(Hash)
+
+      attributes = munchie[:data][:attributes]
+    
+      expect(attributes).to have_key(:destination_city)
+      expect(attributes[:destination_city]).to be_a(String)
+      expect(attributes).to have_key(:travel_time)
+      expect(attributes[:travel_time]).to be_a(String)#hours mins
+      expect(attributes).to have_key(:forecast)
+      expect(attributes[:forecast]).to be_a(Hash)
+      expect(attributes[:forecast]).to have_key(:summary)
+      expect(attributes[:forecast][:summary]).to be_a(String)
+      expect(attributes[:forecast]).to have_key(:temperature)
+      expect(attributes[:forecast][:temperature]).to be_a(Numeric)
+      expect(attributes).to have_key(:restaurant)
+      expect(attributes[:restaurant]).to be_a(Hash)
+      expect(attributes[:restaurant]).to have_key(:name)
+      expect(attributes[:restaurant][:name]).to be_a(String)
+      expect(attributes[:restaurant]).to have_key(:address)
+      expect(attributes[:restaurant][:address]).to be_a(String)
     end
   end
 end
