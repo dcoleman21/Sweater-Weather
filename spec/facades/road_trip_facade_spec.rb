959458d46acc0b4e8388391a70e93fba4852bd1e
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 19:03:41 -0700

    Add a before each do to create a user. Mock out requests

--- spec/facades/road_trip_facade_spec.rb
+++ spec/facades/road_trip_facade_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 describe 'Road Trip Facade' do
-    origin = 'Arvada,CO'
-    destination = 'ApolloBeach,FL'
-    api_key = SecureRandom.hex
-
-    User.create!( email: 'someone@email.com',
+  before :each do
+    User.create!( email: 'dani@email.com',
                   password: 'loveme',
                   password_confirmation: 'loveme',
-                  api_key: api_key
+                  api_key: SecureRandom.hex
                 )
-
+  end
 
   it 'returns a road trip poro when given an origin and destination city' do
+
     json1 = File.read('spec/fixtures/route_data_arvada_apollo_beach.json')
     stub_request(:get, "http://www.mapquestapi.com/directions/v2/route?from=Arvada,CO&key=#{ENV['MAP_QUEST_KEY']}&to=ApolloBeach,FL").
        with(
@@ -23,7 +21,7 @@ describe 'Road Trip Facade' do
          }).
        to_return(status: 200, body: json1, headers: {})
 
-    json_response = File.read('spec/fixtures/route_data_apollo_beach.json')
+    json_response = File.read('spec/fixtures/map_data_apollo_beach.json')
     stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAP_QUEST_KEY']}&location=ApolloBeach,FL").
       with(
         headers: {
@@ -37,7 +35,7 @@ describe 'Road Trip Facade' do
       stub_request(:get, "https://api.openweathermap.org/data/2.5/onecall?appid=#{ENV['OPEN_WEATHER_KEY']}&lat=27.763584&lon=-82.40031&exclude=minutely,alerts&units=imperial")
         .to_return(status: 200, body: json2, headers: {})
 
-    trip_details = RoadTripFacade.trip_info(origin, destination)
+    trip_details = RoadTripFacade.trip_info('Arvada,CO', 'ApolloBeach,FL')
 
     expect(trip_details).to be_a(RoadTrip)
     expect(trip_details.start_city).to eq("Arvada,CO")
@@ -53,16 +51,17 @@ describe 'Road Trip Facade' do
   it "returns a road trip poro with impossible route when origin and destination are impossible" do
     origin = 'NewYork,NY'
     destination = 'Perth,AUS'
-    api_key = SecureRandom.hex
-
-    User.create!( email: 'bob@example.com',
-                  password: 'travel',
-                  password_confirmation: 'travel',
-                  api_key: api_key
-                )
-
 
     json_response = File.read('spec/fixtures/impossible_route.json')
+    stub_request(:get, "http://www.mapquestapi.com/directions/v2/route?from=NewYork,NY&key=#{ENV['MAP_QUEST_KEY']}&to=Perth,AUS").
+        with(
+          headers: {
+         'Accept'=>'*/*',
+         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+         'User-Agent'=>'Faraday v1.3.0'
+          }).
+        to_return(status: 200, body: json_response, headers: {})
+    json1 = File.read('spec/fixtures/impossible_route.json')
     stub_request(:get, "http://www.mapquestapi.com/geocoding/v1/address?key=#{ENV['MAP_QUEST_KEY']}&location=Perth,AUS").
          with(
            headers: {
@@ -70,7 +69,7 @@ describe 'Road Trip Facade' do
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'User-Agent'=>'Faraday v1.3.0'
            }).
-         to_return(status: 200, body: json_response, headers: {})
+         to_return(status: 200, body: json1, headers: {})
 
     trip_details = RoadTripFacade.trip_info('NewYork,NY', 'Perth,AUS')
 
