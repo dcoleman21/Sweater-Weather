c700e1949811a13a3c28757639d4b9816dd1ebb0
Dani Coleman <danicoleman00@gmail.com>
2021-01-19 19:02:50 -0700

    Refactor road trip facade to use defensive coding with a single exit point

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,8 +1,11 @@
 class RoadTripFacade
   def self.trip_info(origin, destination)
     travel_time = MapFacade.time_between_locations(origin, destination)
-    coords = MapFacade.get_coords_by_location(destination)
-    weather_info = ForecastService.forecast_by_coords(coords[:lat], coords[:lng])
+    weather_info = ''
+    if travel_time != 'impossible route'
+      coords = MapFacade.get_coords_by_location(destination)
+      weather_info = ForecastService.forecast_by_coords(coords[:lat], coords[:lng])
+    end
     RoadTrip.new(origin, destination, travel_time, weather_info)
   end
 end
